4a3423248c79a055f15e071f06853d09cf6ade86
MinchMeister <minch.bradley@gmail.com>
2019-02-02 21:30:29 -0500

    init commit

---
+++ Core2/Core2/Controllers/BaseController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core2.Controllers
+{
+    public class BaseController : Controller
+    {
+        //graphql => look this up
+        //cqrs pattern
+        //event sourcing pattern
+
+        //learn react.js and maybe redux.js
+
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+        public IActionResult PageNotFound()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+
+
+    }
+}
