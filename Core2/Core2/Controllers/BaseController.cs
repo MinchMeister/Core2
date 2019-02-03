c66b617c50790aae78ca0a90ab04d4df76f321d4
MinchMeister <minch.bradley@gmail.com>
2019-02-02 23:44:40 -0500

    TODO list

--- Core2/Core2/Controllers/BaseController.cs
+++ Core2/Core2/Controllers/BaseController.cs
@@ -8,24 +8,6 @@ namespace Core2.Controllers
 {
     public class BaseController : Controller
     {
-        //graphql => look this up
-        //cqrs pattern
-        //event sourcing pattern
-
-        //learn react.js and maybe redux.js
-
-        //sass
-
-        //react.js
-        //redux.js
-
-
-        //middleware
-            //middleware in .net app
-
-        //what is bower => front end package manager
-
-
         public IActionResult Logout()
         {
             return View();
